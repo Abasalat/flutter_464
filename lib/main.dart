1aa16b69f1ec2f5bd3ea35db80b5599752c5c0b4
Abasalat <abasalat.bscsf21@iba-suk.edu.pk>
2025-07-12 13:29:42 +0500

    create the login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,160 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue.shade300,
-        appBar: AppBar(
-          backgroundColor: Colors.lightBlueAccent.shade100,
-          title: Text('my app oo'),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    height: 50,
+                    width: 50,
+                    image: AssetImage('assets/logo.png'),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Maintenance',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Rubik Medium',
+                          color: Color(0xff2D3142),
+                        ),
+                      ),
+                      Text(
+                        'Box',
+                        style: TextStyle(
+                          fontFamily: 'Rubik Medium',
+                          fontSize: 24,
+                          color: Colors.deepOrange,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 14),
+              Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: 'Rubik Medium',
+                    color: Color(0xff2D3142),
+                  ),
+                ),
+              ),
+              SizedBox(height: 14),
+              Center(
+                child: Text(
+                  'This is my login screeen with good \n ui n and good user experience',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Rubik Regular',
+                    color: Color(0xff4C5980),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 30),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    prefixIcon: Icon(Icons.email, color: Color(0xff323F4B)),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    prefixIcon: Icon(Icons.lock, color: Color(0xff323F4B)),
+                    suffixIcon: Icon(Icons.visibility_off_outlined),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 100),
+              Container(
+                height: 50,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Rubik Regular',
+                      color: Colors.white,
+                    ),
+                    'Log in',
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Dont have an account?',
+                    style: TextStyle(
+                      fontFamily: 'Rubik Regular',
+                      fontSize: 16,
+                      color: Color(0xff4C5980),
+                    ),
+                  ),
+                  Text(
+                    'Sign up',
+                    style: TextStyle(
+                      fontFamily: 'Rubik Medium',
+                      fontSize: 16,
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        body: const Center(child: Image(image: AssetImage('assets/Me3.png'))),
       ),
-    ),
-  );
+    );
+  }
 }
